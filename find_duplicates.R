cf926fe139ef9e40f49563a9c4402a93e5a4009c
Rory Quinn <quinnroryj@hotmail.com>
2020-04-29 21:18:48 +0100

    Update find_duplicates.R

--- find_duplicates.R
+++ find_duplicates.R
@@ -2,4 +2,7 @@ FindDuplic <- function (x)
 {
     duplicated(x) | duplicated(x, fromLast = TRUE)
 }
+
+FindDuplic (x)
+
 # if the data contains NA Values and you don't want to include these in the matches, add the argument Incomparables=NA to the function
